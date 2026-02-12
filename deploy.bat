ebb241a2a169f6797a1c58cbfe4a3ccf4384ee2b
Jackpenny25 <166762163+Jackpenny25@users.noreply.github.com>
2026-02-12 17:12:53 +0000

    Fixing the deploy file

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,14 @@
 @echo off
+if /i "%~1" neq "fromcmd" (
+	start "" cmd /k "%~f0" fromcmd
+	exit /b
+)
 cd /d C:\Users\Admin\Desktop\Flight-Points\Code\Flight-Points
 git pull
 npm install
 npm run build
 net stop cadet-website
 net start cadet-website
+echo.
+echo Press any key to close...
+pause
